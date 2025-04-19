9131dfb725080debd05e8f8ae7239671426ad434
ranahunter10 <ashishrana1619@gmail.com>
2025-04-20 03:31:08 +0530

    some priduction grade set ups

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+  }
+}
+
+
+export { asyncHandler }
+
+// for understanding purpose ---->
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
