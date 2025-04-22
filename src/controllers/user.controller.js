5ea27dfd522d9f1742d88f12412eb2298e9f4d82
ranahunter10 <ashishrana1619@gmail.com>
2025-04-22 13:42:36 +0530

    changeCurrentPassword bug resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -452,7 +452,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
   const { oldPassword, newPassword } = req.body;
-  const user = await User.findById(req.user?._id);
+  const user = await User.findById(req.user?._id).select("+password");
 
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
 
